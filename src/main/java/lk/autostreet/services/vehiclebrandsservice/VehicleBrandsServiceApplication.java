b08b6386e1d626607bc271674b4036ef257b6410
chathuranga tennakoon <chathuranga@chathurangas-MacBook-Pro.local>
2019-07-28 12:45:38 +0530

    first commit

---
+++ src/main/java/lk/autostreet/services/vehiclebrandsservice/VehicleBrandsServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.autostreet.services.vehiclebrandsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleBrandsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleBrandsServiceApplication.class, args);
+	}
+
+}
